4147cd11f727225618437c73b3992e15709421ad
heehminh <mink141416@gmail.com>
2023-06-03 17:38:36 +0900

    04_타입과 타입변환_타입변환(문자->숫자)

---
+++ Basic/04_타입과_타입변환/타입변환/main.cpp
@@ -0,0 +1,17 @@
+// 문자를 숫자로, 숫자를 문자로 (좌표이동)
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    char a = 'a';
+    cout << (int)a << "\n";
+    cout << (int)a - 97 << "\n";
+    cout << (int)a - 'a' << "\n";
+    return 0;
+}
+
+/*
+97
+0
+0
+*/
