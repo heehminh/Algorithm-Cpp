09410193eb43d795b96137ff2348ed5de6e143de
heehminh <mink141416@gmail.com>
2023-06-28 21:00:46 +0900

    08_자료구조 (5) set

---
+++ Basic/08_자료구조/set/main.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    set<pair<string, int>> st;
+    st.insert({"test", 1});
+    st.insert({"test", 1});
+    st.insert({"test", 1});
+    st.insert({"test", 1});
+
+    cout << st.size() << "\n"; // 1
+
+    set<int> st2;
+    st2.insert(2);
+    st2.insert(1);
+    st2.insert(2);
+
+    for (auto it : st2) {
+        cout << it << "\n"; // 1 2 
+    }
+
+    return 0;
+}
