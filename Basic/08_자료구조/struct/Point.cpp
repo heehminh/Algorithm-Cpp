9e2ca1adbf3d4cc28430087d6b6e1036bffdab37
heehminh <mink141416@gmail.com>
2023-07-01 19:27:38 +0900

    05_자료구조 (8) struct

---
+++ Basic/08_자료구조/struct/Point.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Point {
+    // 1. 구조체의 멤버변수 y, x를 정의한다. 
+    int y, x;
+    
+    // 2. y, x를 받아 멤버변수를 생성한다. 
+    Point(int y, int x): y(y), x(x) {}
+
+    // 3. 초기값 설정 (y, x가 정해지지 않은 경우 기본값 -1, -1로 설정한다.)
+    Point(){y=-1; x=-1;}
+
+    // 4. 연산자 오버로딩 
+    bool operator < (const Point& a) const {
+        if (x==a.x) return y < a.y;
+        return x < a.x;
+    }
+};
+
+int main() {
+
+    return 0;
+}
---
+++ Basic/08_자료구조/struct/Ralo.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Ralo {
+    int a, b;
+    double c, d, e;
+};
+
+void print(Ralo ralo) {
+    cout << ralo.a << " " << ralo.b << " " << ralo.c << " " << ralo.d << " " << ralo.e << "\n";
+}
+
+int main() {
+    Ralo ralo = {1, 1, 1, 1, 1};
+    print(ralo);
+
+    vector<Ralo> ret;
+    ret.push_back({1, 2, 3, 4, 5});
+    ret.push_back({1, 2, 3, 4, 6});
+    ret.push_back({}); // 값을 집어넣지 않은 경우 0으로 초기화
+    ret.push_back({1, 3});
+    
+    for (Ralo ralo : ret) {
+        print(ralo);
+    }
+
+    return 0;
+}
